b32698a63de12a94961ff76dd3344400446a143c
joldiazch <jluis.diaz@udea.edu.co>
2020-02-15 14:59:03 +0000

    no check task 3, add __init method

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 
 import uuid
+from datetime import datetime
 
 class BaseModel():
+    """ BaseModel Class """
 
-        id = None
+    def __init__(self, *args, **kwargs):
+        """  """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+    def save(self):
+        """ save method """
+        updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+    def to_dict(self):  
+        """ to_dict method """
+        return self.__dict__
+
+    def __str__(self):
+        """ __str__ method """
+        msg = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return msg
+        
