93f01b33a9beb5dcf318a90a3fbb927a4d91ea82
jdrestre <jdrestre79@gmail.com>
2020-02-15 11:13:25 -0500

    continue task 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,19 +6,22 @@ from datetime import datetime
 class BaseModel():
     """ BaseModel Class """
 
-    def __init__(self, *args, **kwargs):
-        """  """
+    def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-        self.updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """ save method """
-        updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        updated_at = datetime.now()
 
-    def to_dict(self):  
+    def to_dict(self):
         """ to_dict method """
-        return self.__dict__
+        dic = self.__dict__
+        dic ['__class__'] = self.__class__.__name__
+        dic ['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        dic ['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return dic
 
     def __str__(self):
         """ __str__ method """
