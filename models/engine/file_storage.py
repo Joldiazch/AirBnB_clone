aab7943f2ba78a371158a972b73b404a2a1c9e5a
jdrestre <jdrestre79@gmail.com>
2020-02-15 14:35:14 -0500

    update file file_storage and create __init__ into file /models/engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-
-""" coment here """
+"""Contain the class FileStorage"""
+from models.base_model import BaseModel
+import json
+import os
 
 class FileStorage():
 	""" FileStorage Class """
 
-	def __init__(self):
-		"""  """
-		self.__file_path = "json_storage.js"
-		self.__objects = {}
+        __file_path = "file.json"
+        __objects = {}
 
 	def all(self):
-		"""  """
-		pass
+		"""Return all objects from dictionary __objects"""
+		return self.__objects
 
 	def new(self, obj):
-		"""  """
-		pass
-	
+		"""Set in __objects with key <obj class name>.id"""
+		self.__objects["{}.{}".format(obj.__class__.__name__, obj.id] = obj
+
 	def save(self):
-		"""  """
-		pass
-	
+		"""serializes __objects to the JSON file (path: __file_path)"""
+		with open(self.__file_path, "w", enconding="UTF-8") as file:
+                               obj_dict = {name_attr: value.to_dict() for name_attr, value in self.__objects.items()}
+                               json.dump(obj_dict, file)
 	def reload(self):
-		"""  """
-		pass
-	
+                """deserialization the JSON file to __objects, only JS"""
