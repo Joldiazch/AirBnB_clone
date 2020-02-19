c24aecd88ade2cde34abd5c6d6e13585828e63c9
jdrestre <jdrestre79@gmail.com>
2020-02-19 14:59:57 -0500

    update create_ad in base model

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def save(self):
